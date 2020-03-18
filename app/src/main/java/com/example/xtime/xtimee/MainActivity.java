1059b17d365b2d9ce4c8c62228e81a5e375d6c60
changheng302 <changheng302@163.com>
2020-03-18 16:07:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xtime/xtimee/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.xtime.xtimee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    //TODO finish this code
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
